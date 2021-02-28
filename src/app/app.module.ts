296aec346f4fca87768207879a95664f128919dc
sachiniMuthugala <sachinimuthugala99@gmail.com>
2021-02-28 22:31:03 +0530

    pdf Generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwFormComponent } from './tw-form/tw-form.component';
+import { TwReportComponent } from './tw-report/tw-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwFormComponent,
+    TwReportComponent
   ],
   imports: [
     BrowserModule,
