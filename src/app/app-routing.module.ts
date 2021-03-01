8cc5258c9746e46aee0dd5bed24f1ad9d864952a
Hashini8 <hashdslv@gmail.com>
2021-03-01 21:08:01 +0530

    Added the sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SamplePage1Component } from './sample-page1/sample-page1.component';
+import { SamplePage2Component } from './sample-page2/sample-page2.component';
+import { SamplePage3Component } from './sample-page3/sample-page3.component';
+import { TestPageComponent } from './test-page/test-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TestPageComponent, children: [
+    { path: 'page1', component: SamplePage1Component},
+    { path: 'page2', component: SamplePage2Component},
+    { path: 'page3', component: SamplePage3Component},
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { TestPageComponent } from './test-page/test-page.component';
+import { SamplePage1Component } from './sample-page1/sample-page1.component';
+import { SamplePage2Component } from './sample-page2/sample-page2.component';
+import { SamplePage3Component } from './sample-page3/sample-page3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    TestPageComponent,
+    SamplePage1Component,
+    SamplePage2Component,
+    SamplePage3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
