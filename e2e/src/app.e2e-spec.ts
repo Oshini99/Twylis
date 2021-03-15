8e9c791f2fb873aedccfaef40d316b9365fe4463
AmshikaH <66556626+AmshikaH@users.noreply.github.com>
2021-03-16 01:49:29 +0530

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('Loginpage is running!');
+    expect(await page.getTitleText()).toEqual('twylis-angular app is running!');
   });
 
   afterEach(async () => {
