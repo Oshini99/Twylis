d41513e69c599fb7b9d162b25fe571cc666abb5e
Amshika <amshikahameed@gmail.com>
2021-03-16 12:10:34 +0530

    Sachini's report components

---
+++ src/app/tw-form/tw-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tw-form',
+  templateUrl: './tw-form.component.html',
+  styleUrls: ['./tw-form.component.css']
+})
+export class TwFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/tw-report/tw-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tw-report',
+  templateUrl: './tw-report.component.html',
+  styleUrls: ['./tw-report.component.css']
+})
+export class TwReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
