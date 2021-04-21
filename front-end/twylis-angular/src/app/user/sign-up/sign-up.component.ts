f568da791af1a63ed243c1e4bbac85a9667b7332
Amshika <amshikahameed@gmail.com>
2021-04-21 13:12:58 +0530

    sign-up/sign-in backend

--- front-end/twylis-angular/src/app/user/sign-up/sign-up.component.ts
+++ front-end/twylis-angular/src/app/user/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from '../../shared/user.model';
 import { UserService } from '../../shared/user.service';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,7 +12,7 @@ import { UserService } from '../../shared/user.service';
 })
 export class SignUpComponent implements OnInit {
   user!: User;
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
   userService: any;
   toastr: any;
   sub: any;
@@ -19,49 +21,68 @@ export class SignUpComponent implements OnInit {
   //userService: any;
   //toastr: any;
 
-  constructor() { }
+  registerUserData = {
+    email: undefined,
+    password: undefined
+  };
+  constructor(private _authNew: AuthService,
+              private _router: Router) { }
 
-  googleloginfunction(){
-    this.sub = this._auth.login("google").subscribe(
-      (data: User) => {console.log(data);this.user=data;return this.router.navigateByUrl(this.returnUrl);}
-    );
+
+  // googleloginfunction(){
+  //   this.sub = this._auth.login("google").subscribe(
+  //     (data: User) => {console.log(data);this.user=data;return this.router.navigateByUrl(this.returnUrl);}
+  //   );
+  // }
+
+  // facebookloginfunction(){
+  //   this.sub = this._auth.login("facebook").subscribe(
+  //     (data: User) => {alert(JSON.stringify(data));this.user=data;return this.router.navigateByUrl(this.returnUrl);}
+  //   )
+  // }
+
+  // returnUrl(returnUrl: any) {
+  //   throw new Error('Method not implemented.');
+  // }
+  ngOnInit() {
+    // this.resetForm();
   }
 
-  facebookloginfunction(){
-    this.sub = this._auth.login("facebook").subscribe(
-      (data: User) => {alert(JSON.stringify(data));this.user=data;return this.router.navigateByUrl(this.returnUrl);}
+  registerUser() {
+    this._authNew.registerUser(this.registerUserData)
+    .subscribe(
+      res => {
+        localStorage.setItem('token', res.token);
+        this._router.navigate(['/account']);
+      },
+      // res => console.log(res),
+      err => console.log(err)
     )
   }
 
-  returnUrl(returnUrl: any) {
-    throw new Error('Method not implemented.');
-  }
-  ngOnInit() {
-    this.resetForm();
-  }
+//   resetForm(form?: NgForm) {
+//     if (form != null)
+//       form.reset();
+//     this.user = {
+//       UserName: '',
+//       Password: '',
+//       Email: '',
+//       FirstName: '',
+//       LastName: ''
+//     }
+//   }
 
-  resetForm(form?: NgForm) {
-    if (form != null)
-      form.reset();
-    this.user = {
-      UserName: '',
-      Password: '',
-      Email: '',
-      FirstName: '',
-      LastName: ''
-    }
-  }
+//   OnSubmit(form: NgForm) {
+//     this.userService.registerUser(form.value)
+//       .subscribe((data: any) => {
+//         if (data.Succeeded == true) {
+//           this.resetForm(form);
+//           this.toastr.success('User registration successful');
+//         }
+//         else
+//           this.toastr.error(data.Errors[0]);
+//       });
+//   }
 
-  OnSubmit(form: NgForm) {
-    this.userService.registerUser(form.value)
-      .subscribe((data: any) => {
-        if (data.Succeeded == true) {
-          this.resetForm(form);
-          this.toastr.success('User registration successful');
-        }
-        else
-          this.toastr.error(data.Errors[0]);
-      });
-  }
-
+// }
 }
